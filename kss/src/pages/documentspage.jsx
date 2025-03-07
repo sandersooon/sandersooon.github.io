d87f438d38b07c65f8ed5b7b4b8a5cbf1d19d792
sandersooon <50462166+sandersooon@users.noreply.github.com>
2025-03-07 22:49:20 +0200

    Create documentspage.jsx

---
+++ kss/src/pages/documentspage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DocumentsPage() {
+  return (
+    <div>
+      <h1>Документи</h1>
+      <p>Регламент, статут та інші документи.</p>
+    </div>
+  );
+}
+
+export default DocumentsPage;
