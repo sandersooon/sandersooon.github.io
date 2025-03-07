8a1293b7b04867a243b6409a1a415640686a1a88
sandersooon <50462166+sandersooon@users.noreply.github.com>
2025-03-07 23:11:47 +0200

    Create events.php

---
+++ kss/events.php
@@ -0,0 +1,26 @@
+<?php include 'includes/header.php'; ?>
+
+<main>
+    <section class="events">
+        <h2>Майбутні Події</h2>
+        <div class="event">
+            <img src="assets/images/event1.jpg" alt="Змагання з авіації" class="event-image">
+            <div class="event-details">
+                <h3>Змагання з авіації</h3>
+                <p>20 червня 2025 року наша академія організує великі змагання з авіації серед студентів і курсантів.</p>
+                <a href="#">Дізнатись більше</a>
+            </div>
+        </div>
+
+        <div class="event">
+            <img src="assets/images/event2.jpg" alt="Конференція курсантів" class="event-image">
+            <div class="event-details">
+                <h3>Конференція курсантів</h3>
+                <p>10 червня 2025 року ми проведемо конференцію, присвячену останнім досягненням в галузі авіації.</p>
+                <a href="#">Дізнатись більше</a>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include 'includes/footer.php'; ?>
