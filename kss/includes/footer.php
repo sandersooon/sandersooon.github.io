59ac35f4c7a3affe60937ab3c551c314b60c471c
sandersooon <50462166+sandersooon@users.noreply.github.com>
2025-03-07 23:13:02 +0200

    Create footer.php

---
+++ kss/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <p>&copy; 2025 Льотна академія | Все права захищено</p>
+    <div class="social-links">
+        <a href="#">Facebook</a>
+        <a href="#">Instagram</a>
+        <a href="#">Twitter</a>
+    </div>
+</footer>
+</body>
+</html>
