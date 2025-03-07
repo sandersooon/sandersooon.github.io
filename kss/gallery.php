f9149577dc7adb2e9afd3ad2c971a0b83ce47180
sandersooon <50462166+sandersooon@users.noreply.github.com>
2025-03-07 23:12:05 +0200

    Create gallery.php

---
+++ kss/gallery.php
@@ -0,0 +1,13 @@
+<?php include 'includes/header.php'; ?>
+
+<main>
+    <section class="gallery">
+        <h2>Галерея</h2>
+        <div class="gallery-images">
+            <img src="assets/images/gallery1.jpg" alt="Фотографія з події 1">
+            <img src="assets/images/gallery2.jpg" alt="Фотографія з події 2">
+        </div>
+    </section>
+</main>
+
+<?php include 'includes/footer.php'; ?>
