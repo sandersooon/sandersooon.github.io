ff7f49d38b871fdc26343c84d9d722ef91c2e808
sandersooon <50462166+sandersooon@users.noreply.github.com>
2025-03-07 22:47:26 +0200

    Create footer.jsx

---
+++ kss/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-blue-600 p-4 text-white">
+      <p>&copy; 2025 Курсантсько-студентське самоврядування Летної академії</p>
+    </footer>
+  );
+}
+
+export default Footer;
