f7fb298d2eb1b8a95c68c34628eee5d427018715
sandersooon <50462166+sandersooon@users.noreply.github.com>
2025-03-07 22:48:36 +0200

    Create aboutpage.jsx

---
+++ kss/src/pages/aboutpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>Про нас</h1>
+      <p>Історія та місія курсантсько-студентського самоврядування.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
