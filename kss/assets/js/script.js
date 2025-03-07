cd02b3d117e2009edc1c099c62cdd8800f259e4a
sandersooon <50462166+sandersooon@users.noreply.github.com>
2025-03-07 23:14:23 +0200

    Create script.js

---
+++ kss/assets/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.querySelector(".btn");
+    btn.addEventListener("click", function() {
+        alert("Ви перейшли до подій!");
+    });
+});
